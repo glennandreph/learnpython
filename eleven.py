a98d9c5f01cec13d181d701597c3da8c31fe3711
Glenn Andre Parsons Halvorsen <glennandreph@gmail.com>
2017-12-16 18:50:51 +0100

    Task 10 and 11 complete

---
+++ eleven.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3]
+string = ["hello", "world"]
+names = ["John", "Eric", "Jessica"]
+
+second_name = ["Eric"]
+
+print(numbers)
+print(string)
+print("The second name on the list is {}".format(names[1]))
